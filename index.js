8c899faab10fa3bd948cdad1020a635d8ed5256d
samantha debenedictis <samdebenedictis@aol.com>
2023-02-28 09:56:58 -0500

    commit tree 3d22fb4f45e038ed91150751d8d21951d0ee340b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue>42){
+       return someValue-42
+    }
+    else {
+        return 42-someValue
+    }
+  }
+  function distanceFromHqInFeet(someValue)  {
+     if ((start-destination)*264) {
+         return
+     }
+  }
